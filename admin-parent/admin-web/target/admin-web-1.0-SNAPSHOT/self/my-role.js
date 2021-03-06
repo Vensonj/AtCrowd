3481a76b81d5325aee472a11eed4c26e0dfdbbd7
ven <wenj0826@163.com>
2021-03-06 11:31:28 +0800

    2021-03-06

--- admin-parent/admin-web/target/admin-web-1.0-SNAPSHOT/self/my-role.js
+++ admin-parent/admin-web/target/admin-web-1.0-SNAPSHOT/self/my-role.js
@@ -48,7 +48,7 @@ function fillTBody(pageInfo) {
         var checkboxTd = "<td><input type='checkbox'></td>";
         var roleNameTd = "<td>" + roleName + "</td>";
         var checkBtn = "<button type='button' class='btn btn-success btn-xs'><i class='glyphicon glyphicon-check'></i></button>";
-        var pencilBtn = "<button type='button' class='btn btn-primary btn-xs'><i class='glyphicon glyphicon-pencil'></i></button>";
+        var pencilBtn = "<button id='" + roleId + "' type='button' class='btn btn-primary btn-xs pencilBtn'><i class='glyphicon glyphicon-pencil'></i></button>";
         var removeBtn = "<button type='button' class='btn btn-danger btn-xs'><i class='glyphicon glyphicon-remove'></i></button>";
         var buttonTd = "<td>" + checkBtn + " " + pencilBtn + " " + removeBtn + "</td>";
         var tr = "<tr>" + numberTd + checkboxTd + roleNameTd + buttonTd + "</tr>";
